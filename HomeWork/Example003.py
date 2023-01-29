cd4c773079faf33ebf65252b3bddd129a9ce9ee1
Slava <mc_slava2@mail.ru>
2023-01-29 11:52:05 +0300

    add Example004 and fixed 003

--- HomeWork/Example003.py
+++ HomeWork/Example003.py
@@ -11,6 +11,10 @@
 
 n = input("Введите шестизначное число: ")
 
+if int(n) > 999999:
+    print('Ошибка ввода')
+    exit()
+
 sum1 = int(n[0]) + int(n[1]) + int(n[2])
 sum2 = int(n[3]) + int(n[4]) + int(n[5])
 if sum1 == sum2:
