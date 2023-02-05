4c956718f90fd90f142993c21bb2a873e70f4573
Slava <mc_slava2@mail.ru>
2023-02-05 14:38:04 +0300

    add HW 16-20

--- HomeWork/Example018.py
+++ HomeWork/Example018.py
@@ -6,11 +6,6 @@
 # Последняя строка содержит число X
 
 from random import randint
-# size = int(input('Введите размер списка: '))
-# nums_list = [randint(1, size) for _ in range(size)]
-# print(*nums_list)
-# print(nums_list.count(int(input('Введите искомое число: '))))
-
 size = int(input('Количество чисел: '))
 nums_list = [randint(1, size) for _ in range(size)]
 print(*nums_list)
