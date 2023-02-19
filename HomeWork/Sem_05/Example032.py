58abbfcdccd384f40b7ed9fe5977b76c7fb4d392
Slava <mc_slava2@mail.ru>
2023-02-19 11:04:09 +0300

    add 30-32

---
+++ HomeWork/Sem_05/Example032.py
@@ -0,0 +1,14 @@
+# Определить индексы элементов массива (списка),
+# значения которых принадлежат заданному диапазону (т.е. не
+# меньше заданного минимума и не больше заданного
+# максимума)
+
+
+from Module import create_list, indexes_of_numbers
+
+num_list = create_list(int(input('Введите размер массива: ')))
+left_endpoint = int(input('Введите минимум диапазона: '))
+right_endpoint = int(input('Введите максимум диапазона: '))
+
+print(*num_list)
+print(*indexes_of_numbers(num_list, left_endpoint, right_endpoint))
